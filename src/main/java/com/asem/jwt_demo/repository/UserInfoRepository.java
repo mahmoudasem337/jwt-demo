8816c8233deb29fe293aa82657404e98655f9a0e
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2024-09-26 14:57:32 +0300

    added project files

---
+++ src/main/java/com/asem/jwt_demo/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.asem.jwt_demo.repository;
+
+import com.asem.jwt_demo.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+}
